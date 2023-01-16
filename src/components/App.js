e58d80e51b2b58d0bbf557533d63dab89345d6bf
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-16 10:39:43 +0000

    edited

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,12 @@ const decrement= () => {
   setCount((val) => val <= 1 ? 1 : val-1);
 }
 function getState(val){
-  if(val % 3 == 0){
-    return 'fizz';
+  if(val % 3 == 0 && val % 5 == 0){
+    return 'fizzbuzz';
   } else if(val % 5 == 0){
     return 'buzz';
-  }else if(val % 3 == 0 && val % 5 == 0){
-    return 'fizzbuzz';
+  }else if(val % 3 == 0){
+    return 'fizz';
   }else {
     return 'normal';
   }
