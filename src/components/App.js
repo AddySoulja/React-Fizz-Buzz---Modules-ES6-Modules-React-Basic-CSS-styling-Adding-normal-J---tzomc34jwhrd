b8f1d9cc961b8dff5ce8b670612849e29367ae11
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-16 10:27:52 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useState}from 'react'
 import '../styles/App.css';
 const App = () => {
+const [count, setCount] = useState(1);
 
+const increment= () => {
+  setCount((val) => val+1);
+}
+const decrement= () => {
+  setCount((val) => val > 0 ? val-1 : val);
+}
+function getState(val){
+  if(val % 3 == 0){
+    return 'fizz';
+  } else if(val % 5 == 0){
+    return 'buzz';
+  }else if(val % 3 == 0 && val % 5 == 0){
+    return 'fizzbuzz';
+  }else {
+    return 'normal';
+  }
+}
+const state = getState(count);
   
   return (
-    <div id="main">
-
-    </div>
+    <>
+      <div id="main">
+        <button onClick={increment}>Increase</button>
+        <div id="counter" className={state}>{count}</div>
+        <button onClick={decrement}>Decrease</button>
+      </div>
+    </>
   )
 }
 
